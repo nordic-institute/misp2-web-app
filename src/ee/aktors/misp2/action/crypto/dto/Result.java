f77413998b4ce4c224079970c03c69aecc5879d1
Teemu Kurki <teemu.kurki@gofore.com>
2021-01-22 13:33:24 +0200

    MISPDEV-29 Copyright RIA -> NIIS

--- src/ee/aktors/misp2/action/crypto/dto/Result.java
+++ src/ee/aktors/misp2/action/crypto/dto/Result.java
@@ -1,6 +1,6 @@
 /*
  * The MIT License
- * Copyright (c) 2019 Estonian Information System Authority (RIA)
+ * Copyright (c) 2020 NIIS <info@niis.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
