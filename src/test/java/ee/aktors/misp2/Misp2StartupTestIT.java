4ff7f81ebeac44e979afa59cc501f3f7a8fa4680
Teemu Kurki <teemu.kurki@gofore.com>
2021-01-29 17:23:07 +0200

    MISPDEV-8 improved port settings, 2nd failing IT test

--- src/test/java/ee/aktors/misp2/Misp2StartupTestIT.java
+++ src/test/java/ee/aktors/misp2/Misp2StartupTestIT.java
@@ -3,24 +3,43 @@ package ee.aktors.misp2;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
+import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 
 public class Misp2StartupTestIT {
+    private String misp2Port = Objects.requireNonNull(
+            System.getProperty("misp2.it-test.jetty_port"),
+            "misp2.it-test.jetty_port systemproperty needs to be defined"
+    );
+    final String baseUrlString = "http://localhost:"+ misp2Port +"/misp2";
+
 
     @Test
     public void serviceResponds200() throws Exception {
-        URL url = new URL("http://localhost:8080/misp2");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        assertEquals(HttpStatus.OK.value(), connection.getResponseCode());
-    }
 
+        URL url = new URL(baseUrlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        assertEquals("misp2 endpoint should reply status 200",
+                HttpStatus.OK.value(), connection.getResponseCode()
+        );
+        url = new URL(baseUrlString + "misp/admin");
+        connection = (HttpURLConnection) url.openConnection();
+        assertEquals("admin endpoint should reply with status 200",
+                HttpStatus.OK.value(), connection.getResponseCode()
+        );
+    }
+    @Test
+    public void serviceAdminEndpointResponds200() throws Exception {
+
+        URL url = new URL( baseUrlString +"/admin");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        assertEquals("admin endpoint should reply with status 200",
+                HttpStatus.OK.value(), connection.getResponseCode()
+        );
+    }
 
 }
