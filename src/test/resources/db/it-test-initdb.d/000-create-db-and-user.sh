797b174eaf2bce5a8f99ba0da55e89e059ba1700
Teemu Kurki <teemu.kurki@gofore.com>
2021-02-17 16:38:34 +0200

    MISPDEV-8 same db init for both it-test and local profiles

--- src/test/resources/db/it-test-initdb.d/000-create-db-and-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER misp2;
-    CREATE DATABASE misp2db;
-    GRANT ALL PRIVILEGES ON DATABASE misp2db TO misp2;
-EOSQL
-
