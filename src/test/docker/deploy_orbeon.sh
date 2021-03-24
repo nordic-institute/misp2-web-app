555c58b343033e116377e6cba25ea8aa48bdf847
Teemu Kurki <teemu.kurki@gofore.com>
2021-03-24 11:55:59 +0200

    MISPDEV-48 param for CONTAINER_NAME to deploy_orbeon.sh

--- src/test/docker/deploy_orbeon.sh
+++ src/test/docker/deploy_orbeon.sh
@@ -2,19 +2,23 @@
 set -e
 #
 #set -x
-
+CONTAINER_NAME=${1:-tomcat-dev-service}
 TOMCAT_WEB="/usr/local/tomcat/webapps"
-CONTAINER=$(docker ps --filter "name=tomcat-dev-service" --filter "status=running" --format "{{.Names}}")
+CONTAINER=$(docker ps --filter "name=${CONTAINER_NAME}" --filter "status=running" --format "{{.Names}}")
+[[ -z $CONTAINER ]] && echo "No container with name ${CONTAINER_NAME} found" && exit 1
 if [[ ! -e orbeon.war ]] ; then
   cp --verbose ../../../../misp2-install-source/xtee-misp2-orbeon/war/orbeon.war . || (echo "can't find orbeon war" ; exit 1)
 fi
 echo "copy orbeon war to ${TOMCAT_WEB}/"
 # shellcheck disable=SC2086
 docker cp  orbeon.war ${CONTAINER}:${TOMCAT_WEB}/
+echo "waiting for orbeon war to be deployed"
 # shellcheck disable=SC2086
-while ! (docker exec ${CONTAINER} bash -c "ls ${TOMCAT_WEB}/orbeon/" > /dev/null) ; do
+while ! (docker exec ${CONTAINER} bash -c "ls ${TOMCAT_WEB}/orbeon/" > /dev/null 2>&1) ; do
     sleep 1
+    echo -n  "."
 done
+echo ""
 ORBEON_CONFIG=${TOMCAT_WEB}/orbeon/WEB-INF/resources/config/
 echo "copying  $(ls -m orbeon/) to ${ORBEON_CONFIG}"
 # shellcheck disable=SC2086
