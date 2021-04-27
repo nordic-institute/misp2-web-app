ebaa9364ef58dad1df48e5fbae3697183c22d198
Teemu Kurki <teemu.kurki@gofore.com>
2021-04-27 12:06:15 +0300

    MISPDEV-65 Extenal entity access blocked for EchoServlet.java

--- src/main/java/ee/aktors/misp2/servlet/EchoServlet.java
+++ src/main/java/ee/aktors/misp2/servlet/EchoServlet.java
@@ -25,22 +25,19 @@
 
 package ee.aktors.misp2.servlet;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.StringReader;
-
-import javax.servlet.ServletException;
+import ee.aktors.misp2.util.Const;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.commons.codec.binary.Base64;
 import org.apache.logging.log4j.LogManager;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -48,14 +45,16 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import ee.aktors.misp2.util.Const;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Servlet implementation class EchoServlet
  */
 public class EchoServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private String responseData;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -67,10 +66,11 @@ public class EchoServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         response.reset();
         try {
-            responseData = readRequestData(request);
+            String responseData = readRequestData(request);
             String extension = request.getParameter("extension");
             if (extension == null || extension.isEmpty())
                 extension = Const.XML;
@@ -79,57 +79,56 @@ public class EchoServlet extends HttpServlet {
                 response.setHeader(Const.CONTENT_DISPOSITION, contentDisposition);
                 response.setContentType(request.getContentType());
                 BufferedOutputStream os = new BufferedOutputStream(response.getOutputStream());
-                os.write(responseData.getBytes("UTF-8"));
+                os.write(responseData.getBytes(StandardCharsets.UTF_8));
                 os.close();
             } else {
                 byte[] decoded = null;
-                // File file = File.createTempFile("dummy", ".tmp");
                 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+                dbf.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
                 DocumentBuilder db = dbf.newDocumentBuilder();
                 InputSource is = new InputSource();
                 is.setCharacterStream(new StringReader(responseData));
                 Document doc = db.parse(is);
-                // LogManager.getLogger(this.getClass()).debug(responseData);
                 NodeList sections = doc.getElementsByTagName("*");
                 int numSections = sections.getLength();
                 for (int i = 0; i < numSections; i++) {
                     Element section = (Element) sections.item(i);
                     Node title = section.getFirstChild();
                     if (title != null && title.getNodeType() != Node.ELEMENT_NODE) {
-                        // BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(file));
-                        // os.write((title.getNodeValue().getBytes("UTF-8")));
-                        // os.close();
                         decoded = Base64.decodeBase64(title.getNodeValue());
                     }
                 }
                 response.setContentType(Const.MIME_OCTETSTREAM);
                 response.setHeader(Const.CONTENT_DISPOSITION, contentDisposition);
                 BufferedOutputStream output = new BufferedOutputStream(response.getOutputStream());
+                assert decoded != null;
                 output.write(decoded);
                 output.close();
             }
-        } catch (DocumentException e) {
-            LogManager.getLogger(EchoServlet.class).error("Cannot echo the message (DocumentException)");
-        } catch (IOException e) {
-            LogManager.getLogger(EchoServlet.class).error("Cannot echo the message (IOException)");
-        } catch (SAXException e) {
-            LogManager.getLogger(EchoServlet.class).error("Cannot echo the message (SAXException)");
-        } catch (ParserConfigurationException e) {
-            LogManager.getLogger(EchoServlet.class).error("Cannot echo the message (ParserConfigurationException)");
+        } catch (DocumentException | IOException | SAXException | ParserConfigurationException |
+                IllegalArgumentException | DOMException e) {
+            LogManager.getLogger(EchoServlet.class)
+                    .error(
+                            "Cannot echo the message ({})",
+                            e.getClass().getSimpleName()
+                    );
         }
+
 
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         // not implemented
     }
 
-    protected static String readRequestData(HttpServletRequest req) throws IOException, DocumentException {
+    protected static String readRequestData(HttpServletRequest req) throws IOException, DocumentException, SAXException {
         SAXReader xmlReader = new SAXReader();
+        xmlReader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
         org.dom4j.Document xmlDocument = xmlReader.read(req.getInputStream());
         return xmlDocument.asXML();
     }
